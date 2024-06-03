80109426923ab56e47f7d60d7fd07f1baf402e11
Mithelesh <mitheleshv@gmail.com>
2024-06-03 09:35:13 +0530

    New Levels-1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import MemoryGame from './MemoryGame/MemoryGame';
 
--- frontend/src/MemoryGame/MemoryGame.jsx
+++ frontend/src/MemoryGame/MemoryGame.jsx
@@ -5,12 +5,48 @@ const images = require.context('./images');
 const imageList = images.keys().map(image => images(image));
 
 const cardImages = [
-    { 'src': imageList[1], matched: false },
-    { 'src': imageList[2], matched: false },
-    { 'src': imageList[3], matched: false },
-    { 'src': imageList[4], matched: false },
-    { 'src': imageList[5], matched: false },
-    { 'src': imageList[6], matched: false },
+    [
+        { 'src': imageList[1], matched: false },
+        { 'src': imageList[2], matched: false },
+        { 'src': imageList[3], matched: false },
+        { 'src': imageList[4], matched: false }
+    ],
+    [
+        { 'src': imageList[1], matched: false },
+        { 'src': imageList[2], matched: false },
+        { 'src': imageList[3], matched: false },
+        { 'src': imageList[4], matched: false },
+        { 'src': imageList[5], matched: false },
+        { 'src': imageList[6], matched: false },
+    ],
+    [
+        { 'src': imageList[7], matched: false },
+        { 'src': imageList[8], matched: false },
+        { 'src': imageList[9], matched: false },
+        { 'src': imageList[10], matched: false },
+    ],
+    [
+        { 'src': imageList[7], matched: false },
+        { 'src': imageList[8], matched: false },
+        { 'src': imageList[9], matched: false },
+        { 'src': imageList[10], matched: false },
+        { 'src': imageList[11], matched: false },
+        { 'src': imageList[12], matched: false },
+    ],
+    [
+        { 'src': imageList[1], matched: false },
+        { 'src': imageList[2], matched: false },
+        { 'src': imageList[3], matched: false },
+        { 'src': imageList[4], matched: false },
+        { 'src': imageList[5], matched: false },
+        { 'src': imageList[6], matched: false },
+        { 'src': imageList[7], matched: false },
+        { 'src': imageList[8], matched: false },
+        { 'src': imageList[9], matched: false },
+        { 'src': imageList[10], matched: false },
+        { 'src': imageList[11], matched: false },
+        { 'src': imageList[12], matched: false },
+    ]
 ]
 
 const MemoryGame = () => {
@@ -20,9 +56,11 @@ const MemoryGame = () => {
     const [choiceOne, setChoiceOne] = useState(null);
     const [choiceTwo, setChoiceTwo] = useState(null);
     const [disabled, setDisabled] = useState(false);
+    const [found, setFound] = useState(0);
+    const [level, setLevel] = useState(0);
 
     const shuffleCards = () => {
-        const shuffeledCards = [...cardImages, ...cardImages]
+        const shuffeledCards = [...cardImages[level], ...cardImages[level]]
             .sort(() => Math.random() - 0.5)
             .map((card) => ({ ...card, id: Math.random() }))
         setCards(shuffeledCards)
@@ -47,6 +85,7 @@ const MemoryGame = () => {
                 setCards(prevCards => {
                     return prevCards.map(card => {
                         if (card.src === choiceOne.src) {
+                            setFound(found+1)
                             return {...card, matched: true}
                         } else {
                             return card
@@ -58,13 +97,52 @@ const MemoryGame = () => {
                 setTimeout(() => resetTurn(), 1000)
             }
         }
-    }, [choiceOne, choiceTwo]);
+
+        let grid = document.querySelector('.card-grid')
+        if (level === 5) {
+            grid.style.gridTemplateColumns = '1fr 1fr 1fr 1fr 1fr 1fr'
+        }
+
+    }, [choiceOne, choiceTwo, found, level]);
+
+    // useEffect(() => {
+    //     if (turns === cards.length) {
+    //         shuffleCards()
+    //     }
+    // }, [turns])
+    
+    useEffect(() => {
+        console.log(cards);
+        if (found*2 === cards.length && found !== 0) {
+            setTimeout(() => levelCompeted(), 2000)
+        } else {
+            let grid = document.querySelector('.card-grid')
+            grid.style.display = 'grid'
+        }
+    }, [found])
+
+    const levelCompeted = () => {
+        let grid = document.querySelector('.card-grid')
+        let levelBlock = document.querySelector('.level')
+        grid.style.display = 'none'
+        levelBlock.style.display = 'block'
+    }
+    
+    const levelup = () => {
+        setLevel(level + 1)
+        setFound(0)
+        shuffleCards()
+        let grid = document.querySelector('.card-grid')
+        let levelBlock = document.querySelector('.level')
+        grid.style.display = 'grid'
+        levelBlock.style.display = 'none'
+    }
 
     return (
         <div className='MemoryGame'>
             <h1>Memory Match</h1>
             <button onClick={shuffleCards} >New Game</button>
-            <div className="card-grid">
+            <div className='card-grid'>
                 { cards.map(card => (
                     <SingleCard
                         key={card.id} 
@@ -76,7 +154,10 @@ const MemoryGame = () => {
                     />
                 )) }
             </div>
-            <p>Turns: {turns} </p>
+            <p style={{zIndex: '1', color: 'red'}} > <span style={{zIndex: '1'}}> Turns: {turns} </span></p>
+            <div className="level">
+                <button onClick={levelup} >Next Level</button>
+            </div>
         </div>
     )
 }
